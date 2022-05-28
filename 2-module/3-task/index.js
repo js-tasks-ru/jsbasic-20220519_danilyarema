3e052387603a1c980f0666c475cdc18e1fd9e53b
lixodey <danilyarema@gmail.com>
2022-05-28 18:05:31 +0300

    Третья

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,17 @@
-let calculator = {
-  // ваш код
+let calculator = {};
+
+calculator.read = function(a,b){
+  this.a = a;
+  this.b = b;
+};
+
+calculator.mul = function(){
+  return this.a * this.b  
+};
+
+calculator.sum = function(){
+  return this.a + this.b
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+window.calculator = calcuator; // делает ваш калькулятор доступным глобально
