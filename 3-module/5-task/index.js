30a35f8a2ec6995ceae6e9ecc4392b4f0e1e997b
lixodey <danilyarema@gmail.com>
2022-06-02 13:32:44 +0300

    Задачи

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  const out = {};
+  
+  temp = str.split(' ')
+    .map(item => +item)
+    .filter(item => typeof(item) === 'number' && !isNaN(item))
+    .sort( (a,b) => a - b)
+  
+  out.min = temp[0]
+  out.max = temp[temp.length - 1]
+  return out
 }
