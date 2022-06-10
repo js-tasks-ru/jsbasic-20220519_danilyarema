12031206f5aa1c890df09a9909e85e869bf200e1
lixodey <danilyarema@gmail.com>
2022-06-10 14:04:31 +0300

    Изменения

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,5 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const temp = document.body.childNodes[1].childNodes[1]
+  for (let i = 0; i <= 5; i++)
+    temp.rows[i].cells[i].style.backgroundColor = "red"
 }
