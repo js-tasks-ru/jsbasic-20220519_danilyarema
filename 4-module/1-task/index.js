6932e9bd0eeaf50d62cf9cc637290601b9ca28cb
lixodey <danilyarema@gmail.com>
2022-06-10 13:18:11 +0300

    Тестовый

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = ''
+  for (let obj of friends)
+    list += '<li>' + obj.firstName + ' ' + obj.lastName + '</li>'
+  list = ('<ul>' + list + '</ul>')
+  console.log(list)
+  document.head.childNodes[1].insertAdjacentHTML('afterEnd', list)
 }
