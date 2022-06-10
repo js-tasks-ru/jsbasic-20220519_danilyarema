be679841dc43a45e2be51c03c6c0fe9d5b240469
lixodey <danilyarema@gmail.com>
2022-06-10 14:20:33 +0300

    Изменения

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,5 +1,4 @@
 function makeDiagonalRed(table) {
-  const temp = document.body.childNodes[1].childNodes[1]
-  for (let i = 0; i <= 5; i++)
-    temp.rows[i].cells[i].style.backgroundColor = "red"
+  for (let i = 0; i <= table.rows.length; i++)
+    table.rows[i].cells[i].style.backgroundColor = "red"
 }
