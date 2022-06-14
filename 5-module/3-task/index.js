e24200faa500e2e9698acb381370c961ce49a992
lixodey <danilyarema@gmail.com>
2022-06-14 18:00:53 +0300

    Следующий

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,43 @@
 function initCarousel() {
-  // ваш код...
+  const right = document.querySelector('.carousel__arrow_right')
+  const left = document.querySelector('.carousel__arrow_left')
+  const inner = document.querySelector('.carousel__inner')
+  let pos = 0 //счётчик
+  drawArrows();
+
+  function drawArrows() {  
+    switch(pos){
+      case 0: left.style.display = 'none';
+      right.style.display = '';
+      break;
+  
+      case 3: left.style.display = '';
+      right.style.display = 'none';
+      break;
+  
+      default:left.style.display = '';
+      right.style.display = '';
+    };
+  }
+  
+  right.addEventListener('click',(event) => {
+    if (pos <= 2) {
+      pos = pos + 1
+    } 
+    
+    let translateDist =  inner.offsetWidth * pos
+    inner.style.transform = 'translateX(-' + translateDist + 'px)'  
+    drawArrows();
+  })
+
+  left.addEventListener('click', (event) => {
+    if (pos >= 1 ) {
+      pos = pos - 1
+    } 
+    
+    let translateDist =  inner.offsetWidth * pos
+    inner.style.transform = 'translateX(-' + translateDist + 'px)' 
+    drawArrows();
+  })
 }
+
