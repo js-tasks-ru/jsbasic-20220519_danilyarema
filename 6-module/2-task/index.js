9cc4de0a45e4ca351ef75de4aa73e20150eb30e3
lixodey <danilyarema@gmail.com>
2022-06-16 17:21:13 +0300

    Изменения

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -6,24 +6,31 @@ export default class ProductCard {
   }
 }
 function createDOM(elemObj){
-    let DOM = createElement(`<div class="card">
-                                  <div class="card__top">
-                                    <img src="/assets/images/products/${elemObj.image}" class="card__image" alt="product">
-                                    <span class="card__price">€${elemObj.price.toFixed(2)}</span>
-                                  </div>
-                               <div class="card__body">
-                                <div class="card__title">${elemObj.name}</div>
-                                  <button type="button" class="card__button">
-                                  <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-                                  </button>
-                                </div>
-                              </div>`)
-    return DOM                          
+    let DOM = createElement(
+      `<div class="card">
+           <div class="card__top">
+            <img src="/assets/images/products/${elemObj.image}" class="card__image" alt="product">
+            <span class="card__price">€${elemObj.price.toFixed(2)}</span>
+          </div>
+          <div class="card__body">
+            <div class="card__title">${elemObj.name}</div>
+                <button type="button" class="card__button">
+                  <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                </button>
+            </div>
+          </div>`)
+                              
+          let button = DOM.querySelector(".card__button");
+          //console.log(button.className)
+          button.addEventListener('click',  (event) => {
+          button.dispatchEvent(new CustomEvent("product-add"),{
+          detail : elemObj.id,
+          bubbles : true
+          })
+          })
+         // console.log(elemObj.id)
+    return DOM                         
   }
 
-  // new CustomEvent("product-add"), { // имя события должно быть именно "product-add"
-  //   detail: this.product.id, // Уникальный идентификатора товара из объекта товара
-  //   bubbles: true // это событие всплывает - это понадобится в дальнейшем
-// }
 
 
